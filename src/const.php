88529ddfaab66cabe0b74541eeb73e9d00e18312
Laurent Gautho-lapeyre <delphes+github@outlook.fr>
2016-04-03 21:58:34 +0200

    Fichier de configuration

--- src/const.php
+++ src/const.php
@@ -1,31 +1,21 @@
 <?php
-	define('MODE_LOCAL', 'local');
-	define('MODE_OVH', 'ovh');
+    require('api/epiphany/Epi.php');
 
-	$mode = MODE_LOCAL;
-
-	if($mode == MODE_LOCAL) {
-		// Local
-		$con_serv = 'localhost';
-		$con_dbname = 'base';
-		$con_username = 'root';
-		$con_password = '';
-	} elseif($mode == MODE_OVH) {
-		// Distant OVH
-		$con_serv = '';
-		$con_dbname = '';
-		$con_username = '';
-		$con_password = '';
-	} else {
-		throw new Exception('Mode applicatif incorrecte.');
-	}
-
+    Epi::init('config');
+    getConfig()->load('./config.ini');
+    
+	//database
+    $con_serv = getConfig()->get('database')->dbhost;
+	$con_dbname = getConfig()->get('database')->dbname;
+	$con_username = getConfig()->get('database')->dbusername;
+	$con_password = getConfig()->get('database')->dbpassword;
+    
 	// Infos
-	date_default_timezone_set("Europe/Paris");
-	$dateNaissance = mktime(0, 0, 0, 07, 27, 1985);
-	$ville = 'Paris, France';
-	$urlViadeo = 'http://www.viadeo.com/fr/profile/laurent.gautho.lapeyre';
-	$urlLinkedIn = 'http://fr.linkedin.com/in/laurentgautholapeyre';
-	$urlTwitter= 'https://twitter.com/Delphes99';
-	$urlCvPdf = 'cv_laurent_gautho-lapeyre_public.pdf';
+	date_default_timezone_set(getConfig()->get('timezone'));
+	$dateNaissance = strtotime(getConfig()->get('user')->birthday_date);
+	$ville = getConfig()->get('user')->location;
+	$urlViadeo = getConfig()->get('user')->viadeo;
+	$urlLinkedIn = getConfig()->get('user')->linkedin;
+	$urlTwitter= getConfig()->get('user')->twitter;
+	$urlCvPdf = getConfig()->get('user')->pdf_file;
 ?>
